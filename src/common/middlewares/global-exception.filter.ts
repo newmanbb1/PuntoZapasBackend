427c77f0b0c1512b4ee086e1a87b2cfe2e6ebcd5
Sebastian-Cepeda-Ch <alvaro.sebastian.cepeda.choque@gmail.com>
2026-06-01 16:07:23 -0400

    Reparacion de prisma.service.ts (Por ahora funciona prisma), actualizacio de depedencias @prisma/adapter-pg

--- src/common/middlewares/global-exception.filter.ts
+++ src/common/middlewares/global-exception.filter.ts
@@ -7,7 +7,7 @@ import {
     Logger,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { Prisma } from 'generated/prisma/client';
+import { Prisma } from '@prisma/client';
 
 @Catch() // Al no pasar argumentos, atrapa absolutamente TODOS los errores
 export class GlobalExceptionFilter implements ExceptionFilter {
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    // Inicializar Pool de pg y el driver adapter requerido por Prisma 7
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
